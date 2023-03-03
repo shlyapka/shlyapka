b4817db1945a5ac0c93303138be3461c61034811
unknown <dima.molodcov09@gmail.com>
2023-03-03 22:10:34 +0000

    idfjsd

--- themes.js
+++ themes.js
@@ -3,18 +3,29 @@ let blue = document.querySelector('.Blue')
 let anime = document.querySelector('.Anime')
 let purple = document.querySelector('.Purple')
 
+window.myVar = "videos/background.mp4";
+
 const Theme = localStorage.getItem('currentTheme');
 updateUI();
 
 function updateUI(){
 	if (Theme == "green") {
-		document.body.style.setProperty("--accent", "green")
+		document.body.style.setProperty("--accent", "#35823E")
+		document.body.style.setProperty("--time", "#35823E")
+		document.body.style.setProperty("--anim", "none")
+		document.body.style.setProperty("--timebg", "white")
 	}
 	else if (Theme == "blue") {
-		document.body.style.setProperty("--accent", "blue")
+		document.body.style.setProperty("--accent", "#2F386F")
+		document.body.style.setProperty("--time", "#2F386F")
+		document.body.style.setProperty("--anim", "none")
+		document.body.style.setProperty("--timebg", "white")
 	}
 	else if (Theme == "anime") {
 		document.body.style.setProperty("--accent", "gray")
+		document.body.style.setProperty("--time", "gray")
+		document.body.style.setProperty("--anim", "none")
+		document.body.style.setProperty("--timebg", "white")
 	}
 }
 
